df0b900c9257dbf067d1a7cf6485f911b94cc223
Eremin Daniil <danchchik@bk.ru>
2025-11-14 04:21:21 +0000

    Первые наработки

--- cstack.c
+++ cstack.c
@@ -1,42 +1,118 @@
 #include "cstack.h"
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define UNUSED(VAR) (void)(VAR)
 
+struct stack_entries_table g_table = {0u, NULL};
+
 hstack_t stack_new(void)
 {
-    return -1;
+    g_table.size++;                                                                     // увеличиваем размер таблицы стеков
+    g_table.entries = realloc(g_table.entries, g_table.size*sizeof(stack_entry_t));     // выделяем память под элемент таблицы стеков
+
+    // Инициализируем новый стек нулевыми значениями
+    g_table.entries[g_table.size-1].stack = NULL;
+    g_table.entries[g_table.size-1].reserved = 0;
+
+    // Возвращаем размер таблицы меньший на единицу, чтобы было соответствие по индексу
+    return g_table.size-1;
 }
 
 void stack_free(const hstack_t hstack)
 {
-    UNUSED(hstack);
+    if(!(stack_valid_handler(hstack)))                       // проверка на валидность хэндлера
+    {
+        g_table.entries[hstack].reserved = -1;            // помечаем как удаленный
+        stack_type top = g_table.entries[hstack].stack;   // вершина стека
+
+        while(top != NULL)                                // пока не дойдем до дна стека
+        {
+            stack_type tmp = top->prev;                   // временная переменная для хранения указателя на следующий элемент
+            free(top);
+            top = tmp;
+        }
+    }
 }
 
 int stack_valid_handler(const hstack_t hstack)
 {
-    UNUSED(hstack);
-    return 1;
+    if(hstack+1 > (int)(g_table.size) ||                  // выходит за размер таблицы
+       hstack < 0 ||                                      // отрицательные значения
+       g_table.entries[hstack].reserved == -1)            // уже удален
+    {
+        return 1;
+    }
+    
+    return 0;
 }
 
 unsigned int stack_size(const hstack_t hstack)
 {
-    UNUSED(hstack);
+    if(!(stack_valid_handler(hstack)))
+    {
+        return g_table.entries[hstack].reserved;
+    }
     return 0;
 }
 
 void stack_push(const hstack_t hstack, const void* data_in, const unsigned int size)
 {
-    UNUSED(hstack);
-    UNUSED(data_in);
-    UNUSED(size);
+    if(!(stack_valid_handler(hstack)) &&
+        data_in != NULL)
+    {
+        stack_type new = malloc(sizeof(struct node) + size);     // выделяем память под элемент
+        
+        if (new != NULL)
+        {
+            g_table.entries[hstack].reserved += size;         // увеличиваем размер стека
+            new->prev = g_table.entries[hstack].stack;        // Указываем на предыдущий узел
+            new->size = size;                                 // Сохраняем размер данных
+            g_table.entries[hstack].stack = new;              // обновляем вершину
+            
+            // Копируем данные в массив
+            if (data_in != NULL && size > 0) {
+                memcpy(new->data, data_in, size);
+            }
+        }
+
+    }
+    else
+    {
+        //Обозначить ошибку
+    }
 }
 
 unsigned int stack_pop(const hstack_t hstack, void* data_out, const unsigned int size)
 {
-    UNUSED(hstack);
-    UNUSED(data_out);
-    UNUSED(size);
+    if(!(stack_valid_handler(hstack))&&
+        data_out != NULL)
+    {
+        stack_type top = g_table.entries[hstack].stack;          // вершина стека
+        
+        if (top != NULL)
+        {            
+            // Копируем данные в массив
+            if (data_out != NULL && size > 0) {
+                memcpy(data_out, top->data, size);            // копируем данные
+                return size;                                  // возвращаем количество записанных байт
+
+                // Обновляем вершину
+                g_table.entries[hstack].stack = top->prev;
+
+                // Изменяем размер стека
+                g_table.entries[hstack].reserved -= top->size;
+
+                // Убираем вершину
+                free(top);
+            }
+        }
+    }
+    else
+    {
+        //Обозначить ошибку
+    }
     return 0;
 }
 
--- cstack.h
+++ cstack.h
@@ -1,13 +1,51 @@
 #ifndef CSTACK_H
 #define CSTACK_H
 
+#include <stdio.h>
+
 typedef int hstack_t;
 
+struct node
+{
+    struct node* prev;
+    unsigned int size;
+    char data[0];
+};
+
+typedef struct node* stack_type;
+
+struct stack_entry
+{
+    int reserved;
+    stack_type stack;
+};
+
+typedef struct stack_entry stack_entry_t;
+
+struct stack_entries_table
+{
+    unsigned int size;
+    stack_entry_t* entries;
+};
+
+// Создать новый стек.
 hstack_t stack_new(void);
+
+// Удалить стек, если соответствующий хэндлеру стек существует.
 void stack_free(const hstack_t stack);
+
+// Проверить хэндлер.  0 - соответствующий хэндлеру стек существует, 1 - нет.
 int stack_valid_handler(const hstack_t stack);
+
+// Получить количество элементов в стеке, если соответствующий хэндлеру стек существует.
+// Returns: количество элементов в стеке, если соответствующий хэндлеру стек существует, или 0 в противном случае.
 unsigned int stack_size(const hstack_t stack);
+
+// Добавить элемент данных из буфера в стек, если соответствующий хэндлеру стек существует.
 void stack_push(const hstack_t stack, const void* data_in, const unsigned int size);
+
+// Извлечь элемент из стека и записать данные этого элемента в буфер, если соответствующий хэндлеру стек существует.
+// Returns: размер записанных данных в байтах, если соответствующий хэндлеру стек существует, или 0 в противном случае.
 unsigned int stack_pop(const hstack_t stack, void* data_out, const unsigned int size);
 
 #endif /* CSTACK_H */
